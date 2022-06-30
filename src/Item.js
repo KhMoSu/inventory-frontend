3a8aa902031facd32493e1b0682924c460076851
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-06-30 14:10:36 -0700

    styled items in item.js

--- src/Item.js
+++ src/Item.js
@@ -4,13 +4,14 @@ import { Link } from 'react-router-dom';
 export default function Item({ item }) {
   const { bought, amount } = item;
   return (
-    <Link to={'/items/1'}>
-      <div className="items">
-        <h3>
-          Item Name: {item.item} Amount: {amount} Bought: {bought ? 'yes' : 'no'}
-        </h3>
-        {/* <input type='checkbox'>Bought ${bought}</input> */}
-      </div>
-    </Link>
+    // <Link to={'/items/1'}>
+    <div className="items">
+      <h3> Items </h3>
+      <p>Item Name: {item.item} </p>
+      <p>Amount: {amount} </p>
+      <p>Bought: {bought ? 'yes' : 'no'}</p>
+      {/* <input type='checkbox'>Bought ${bought}</input> */}
+    </div>
+    // </Link>
   );
 }
